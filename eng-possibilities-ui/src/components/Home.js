d940f62d43001b2af8868045b9f2344ed48d7f78
Caroline Jin <cljin@mit.edu>
2021-02-25 21:56:54 -0500

    converted from hooks to classes

--- eng-possibilities-ui/src/components/Home.js
+++ eng-possibilities-ui/src/components/Home.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react';
 import "./LineChartExample.js"
 import LineChartExample from "./LineChartExample.js"
 
-function Home(){
-    return (<div>
-                <div>This is the home page</div>
-                <LineChartExample />
-            </div>);
+class Home extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return (<div>This is the home page</div>);
+    }
 }
 
 export default Home;
